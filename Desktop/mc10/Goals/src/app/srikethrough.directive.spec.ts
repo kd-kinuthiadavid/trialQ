28b44c663af9021cd41e7655001371ab86ae1ca9
kd-kinuthiadvid <kd.kinuthiadavid@gmail.com>
2018-05-27 18:54:33 +0300

    fix folder structure

--- Desktop/mc10/Goals/src/app/srikethrough.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { SrikethroughDirective } from './srikethrough.directive';
-
-describe('SrikethroughDirective', () => {
-  it('should create an instance', () => {
-    const directive = new SrikethroughDirective();
-    expect(directive).toBeTruthy();
-  });
-});
