c7b597e35466e6971da581c9cb782d833aba6a7f
kd-kinuthiadvid <kd.kinuthiadavid@gmail.com>
2018-05-27 17:01:09 +0300

    edit components quote and quote-details

--- Desktop/Friday/Quotes/src/app/quote/quote.component.ts
+++ Desktop/Friday/Quotes/src/app/quote/quote.component.ts
@@ -1,15 +1,71 @@
 import { Component, OnInit } from '@angular/core';
+import { Quote } from '../quote';
+import { trigger,style,transition,animate,keyframes,query,stagger } from '@angular/animations';
 
 @Component({
   selector: 'app-quote',
   templateUrl: './quote.component.html',
-  styleUrls: ['./quote.component.css']
+  styleUrls: ['./quote.component.css'],
+  animations: [
+
+    trigger('theQuotes', [
+      transition('* => *',[
+        query(':enter', style({ opacity: 0}), {optional: true}),
+
+        query(':enter', stagger('300ms',[
+          animate('.6s ease-in',keyframes([
+            style({opacity:0, transform: 'translateY(-75%)',offset: 0}),
+            style({opacity:.5, transform: 'translateY(35px)',offset: .3}),
+            style({opacity:1, transform: 'translateY(0)',offset: 1}),
+          ]))]),{optional: true}),
+
+          query(':leave', stagger('300ms',[
+            animate('.6s ease-in',keyframes([
+              style({opacity:1, transform: 'translateY(0%)',offset: 0}),
+              style({opacity:.5, transform: 'translateY(35px)',offset: .3}),
+              style({opacity:0, transform: 'translateY(-75%)',offset: 1}),
+            ]))]),{optional: true}),
+      ])
+    ])
+  ]
 })
 export class QuoteComponent implements OnInit {
+  // myQuote : any= {
+  //   id: 1,
+  //   quote: String,
+  //   by: String
+  // }
+  theQuotes=[new Quote("Remember no one can make you feel inferior without your consent.","Eleanor Roosevelt","Me",new Date(2018,3,17))]
+
+  deleteQuote(isComplete,index) {
+    this.theQuotes.splice(index,1);
+  }
+  receiveQuote(get){
+    get.timeCreated = new Date (get.timeCreated);
+    this.theQuotes.push(get);
+    console.log(this.theQuotes);
+  }
+  public count = 0;
+
+  adding(index){
+    this.theQuotes[index].votes = this.theQuotes[index].votes + 1;
+    if (this.theQuotes[index].votes>this.count) {
+      this.count=this.theQuotes[index].votes;
+    }
+    console.log(this.theQuotes[index].votes);
+  }
+  removing(index){
+    if(this.theQuotes[index].votes > 0){
+      this.theQuotes[index].votes = this.theQuotes[index].votes - 1;
+    } else this.theQuotes[index].votes = 0;
+    console.log(this.theQuotes[index].votes);
+  }
 
   constructor() { }
 
   ngOnInit() {
+    // this.myQuote.quote = "Remember no one can make you feel inferior without your consent.";
+    // this.myQuote.by= "Eleanor Roosevelt";
   }
 
 }
